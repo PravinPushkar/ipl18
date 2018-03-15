a00f85bcdd2feaa28ba34d8a8f57f42a24ec9218
Sushant Mahajan <sushant.mahajan@sap.com>
2018-03-15 15:03:00 +0530

    Added team images and server start log

--- main.go
+++ main.go
@@ -14,5 +14,6 @@ func main() {
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
+	log.Println("Starting server on port 3000...")
 	log.Fatal(http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r)))
 }
