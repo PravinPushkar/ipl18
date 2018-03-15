57b141ab3c834ff1349b369855b8cec35398b057
Sushant Mahajan <sushant.mahajan@sap.com>
2018-03-15 14:29:24 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	log.Fatal(http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r)))
+}
