363eb32441b63bab8cd9ddca8d98f116c747c36a
Sushant Mahajan <sushant.mahajan@sap.com>
2018-03-16 10:53:15 +0530

    added subrouter for better encapsulation

--- backend/middleware.go
+++ backend/middleware.go
@@ -13,7 +13,15 @@ import (
 var SetupAndGetRouter = func() http.Handler {
 	r := mux.NewRouter()
 	setupStatic(r)
-	setupRegistered(r)
+
+	//can be more easily protected
+	//or not using Subrouter
+	pubRouter := r.Path("/pub/").Headers("Content-Type", "application/json").Subrouter()
+	setupPublic(pubRouter)
+
+	apiRouter := r.Path("/api/").Headers("Content-Type", "application/json").Subrouter()
+	setupApi(apiRouter)
+
 	return setupLogging(r)
 }
 
@@ -21,19 +29,21 @@ func setupStatic(r *mux.Router) {
 	//for pages
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-
-	//for registration page
-	r.Handle("/register", handler.NotImplemented).Methods("POST")
 }
 
-func setupRegistered(r *mux.Router) {
-	r.Handle("/profile", handler.NotImplemented).Methods("GET", "POST")
-	r.Handle("/buzz", handler.NotImplemented).Methods("GET", "POST")
-	r.Handle("/jackpot", handler.NotImplemented).Methods("GET", "POST")
-	r.Handle("/voting", handler.NotImplemented).Methods("GET", "POST")
-	r.Handle("/leaderboard", handler.NotImplemented).Methods("GET")
-	r.Handle("/rules", handler.NotImplemented).Methods("GET")
-	r.Handle("/recap", handler.NotImplemented).Methods("GET")
+func setupPublic(r *mux.Router) {
+	r.Handle("/pub/register", handler.NotImplemented).Methods("POST")
+	r.Handle("/pub/info", handler.NotImplemented).Methods("GET")
+}
+
+func setupApi(r *mux.Router) {
+	r.Handle("/api/profile", handler.NotImplemented).Methods("GET", "POST")
+	r.Handle("/api/buzz", handler.NotImplemented).Methods("GET", "POST")
+	r.Handle("/api/jackpot", handler.NotImplemented).Methods("GET", "POST")
+	r.Handle("/api/voting", handler.NotImplemented).Methods("GET", "POST")
+	r.Handle("/api/leaderboard", handler.NotImplemented).Methods("GET")
+	r.Handle("/api/rules", handler.NotImplemented).Methods("GET")
+	r.Handle("/api/recap", handler.NotImplemented).Methods("GET")
 }
 
 func setupLogging(r http.Handler) http.Handler {
