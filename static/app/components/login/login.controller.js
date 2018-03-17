95002a5bcb0bddb4014c7cab6142e6b6ddf01089
I341668 <akshil.verma@sap.com>
2018-03-17 18:05:48 +0530

    Add basic structure for the front-end
    - Add index.html, entry page for the front-end
    - Add directory structure of the application
    - Add modules and routing for the application
    - Add basic views and controllers

---
+++ static/app/components/login/login.controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = angular.module('ipl');
+
+/**
+ * Login Controller
+ * 
+ * Controller for the login page.
+ */
+app.controller('loginController', function(){
+    var vm = this;
+
+    vm.abcd = 'vvv';
+});
